14a4c0baaecefe0c1f3a7073a5531ea297043027
frkayo <mustaphafarouk41@gmail.com>
2022-10-08 14:33:27 +0100

    mobile phase all done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,15 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {
-      colors:{
-        blueBlack:" hsl(233, 26%, 24%)"
-      }
+      colors: {
+        blueBlack: " hsl(233, 26%, 24%)",
+        lightBlue: " hsl(233, 8%, 62%)",
+        LightGrayishBlue: " hsl(220, 16%, 96%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+        LimeGreen: "hsl(136, 65%, 51%)",
+        BrightCyan: " hsl(192, 70%, 51%)",
+      },
+      fontFamily: { PublicSans: "Public Sans" },
     },
   },
   plugins: [],
