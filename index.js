84be7ffdf2ef935ffe265368e146f5a7ea81698c
frkayo <mustaphafarouk41@gmail.com>
2022-10-12 12:37:16 +0100

    All DONE

---
+++ index.js
@@ -0,0 +1,22 @@
+const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
+const nav = document.querySelector(".nav");
+const backdrop = document.querySelector(".backdrop");
+
+hamburger.addEventListener("click", function () {
+  close.classList.toggle("hidden");
+  hamburger.classList.toggle("hidden");
+  nav.classList.toggle("hidden");
+  backdrop.classList.toggle("hidden");
+});
+close.addEventListener("click", function () {
+  close.classList.toggle("hidden");
+  hamburger.classList.toggle("hidden");
+  nav.classList.toggle("hidden");
+  backdrop.classList.toggle("hidden");
+});
+
+backdrop.addEventListener("click", function () {
+  backdrop.classList.add("hidden");
+  nav.classList.toggle("hidden");
+});
